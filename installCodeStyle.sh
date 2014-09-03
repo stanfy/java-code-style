137508fe2709310cf53796ba12c755bb5a5dc591
Roman Mazur <rmazur@stanfy.com.ua>
2014-09-03 17:30:59 +0300

    Add script to install code styles

---
+++ installCodeStyle.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Installs Stanfy's IntelliJ configs into your user configs.
+
+echo "Installing Stanfy code style configs..."
+
+for dir in $HOME/Library/Preferences/IntelliJIdea*/codestyles \
+           $HOME/Library/Preferences/IdeaIC*/codestyles \
+           $HOME/Library/Preferences/AndroidStudio*/codestyles \
+           $HOME/.IntelliJIdea*/config/codestyles \
+           $HOME/.IdeaIC*/config/codestyles \
+           $HOME/.AndroidStudio*/config/codestyles
+do
+  cp -frv $( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )/configs/* $i 2> /dev/null
+done
+
+echo "Done."
+echo ""
+echo "Restart IntelliJ and/or AndroidStudio, go to preferences, and apply 'Stanfy' or 'StanfyAndroid'."
